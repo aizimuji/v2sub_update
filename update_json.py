87fabfb2c3373793c872eb827088c1f040ee802e
aizimuji <129702132+aizimuji@users.noreply.github.com>
2023-08-14 18:11:33 +0800

    更新update_json 模块

--- update_json.py
+++ update_json.py
@@ -5,11 +5,14 @@ import sys
 import json
 
 
-def generate_version_dict(version: str, file_name: str, description: str) -> dict:
+def generate_version_dict(
+    version: str, file_name: str, description: str, with_exec: bool = False
+) -> dict:
     version_dict = {
         "version": version,
-        "file_name": file_name,
+        "update_file": file_name,
         "description": description,
+        "with_exec": with_exec,
     }
 
     return version_dict
@@ -33,15 +36,15 @@ def get_file_url(file_name: str) -> str:
     return fmt_s.format(file_name=file_name)
 
 
-def write_json(version: str):
+def write_json(version: str, with_exec: bool = False):
     win_file_name = f"update_win_{version}.zip"
     description = get_description(version)
     win_file_url = get_file_url(win_file_name)
-    win_dict = generate_version_dict(version, win_file_url, description)
+    win_dict = generate_version_dict(version, win_file_url, description, with_exec)
 
     mac_file_name = f"update_mac_{version}.zip"
     mac_file_url = get_file_url(mac_file_name)
-    mac_dict = generate_version_dict(version, mac_file_url, description)
+    mac_dict = generate_version_dict(version, mac_file_url, description, with_exec)
     json_dict = {"win": win_dict, "mac": mac_dict}
     out_file = "update.json"
     with open(out_file, "w", encoding="utf-8") as f:
@@ -49,5 +52,9 @@ def write_json(version: str):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("usage: update_json version(str) with_exec(1, 0)")
+        sys.exit(1)
     version = sys.argv[1]
-    write_json(version)
+    with_exec = bool(sys.argv[2])
+    write_json(version, with_exec)
